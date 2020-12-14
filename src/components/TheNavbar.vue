9731d19a91af99867c4a13608982ded5eaf9da0d
AndSzy <aszycher@gmail.com>
2020-12-14 18:06:24 +0000

    Bootsrap Added

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-navbar variant="info" type="light">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <b-icon icon="person-fill"></b-icon>
+            </template>
+            <b-dropdown-item to="/login">Login</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item to="/settings">Settings</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <the-navbar></the-navbar>  
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TheNavbar from '../components/TheNavbar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TheNavbar
   }
 }
 </script>
